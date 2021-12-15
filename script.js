3febcacd617fdd672f3af3b8ff7f9720c147e803
Natuuumi <56280643+Natuuumi@users.noreply.github.com>
2021-12-15 16:41:59 -0500

    finished etch a sketch
    added the ability to choose a new grid size and make the trail set a random rgb value.

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
 const container = document.querySelector("#container");
 const reset = document.querySelector("#reset");
+const gridSize = document.querySelector("#gridSize");
+let remove = container.getElementsByClassName("gridWidth");
+gridSize.addEventListener("click", function (e) {
+  while (remove.length > 0) {
+    remove[0].remove();
+  }
 
-reset.addEventListener("click", function (e) {
-  div.style.backgroundColor = "white";
+  let height = prompt("Height 0-100");
+  if (height >= 100) {
+    height = prompt("Height cant be more than 100", 0);
+  }
+  let width = prompt("Width 0-100", height);
+  if (width >= 100) {
+    width = prompt("Width cant be more than 100", 0);
+  }
+  createGrid(height, width);
 });
 
-for (i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.setAttribute("class", "grid");
-  div.addEventListener("mouseover", function (e) {
-    div.style.backgroundColor = "black";
-  });
+function createGrid(height, width) {
+  for (w = 0; w < width; w++) {
+    const widthDiv = document.createElement("div");
+    widthDiv.setAttribute("class", "gridWidth");
+    container.append(widthDiv);
 
-  container.append(div);
-  reset.addEventListener("click", function (e) {
-    div.style.backgroundColor = "white";
-  });
+    for (h = 0; h < height; h++) {
+      const div = document.createElement("div");
+      div.setAttribute("class", "grid");
+      div.addEventListener("mouseover", function (e) {
+        div.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
+      });
+
+      widthDiv.append(div);
+      reset.addEventListener("click", function (e) {
+        div.style.backgroundColor = "white";
+      });
+    }
+  }
 }
+
+function randomRGB() {
+  RGB = Math.floor(Math.random() * 255);
+
+  return RGB;
+}
+
+createGrid(16, 16);
